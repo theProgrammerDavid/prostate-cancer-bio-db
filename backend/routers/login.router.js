52baf0b4391d9eef6aff0133bacba4f677003f83
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-04-05 20:29:41 +0530

    refactor: code

--- backend/routers/login.router.js
+++ backend/routers/login.router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { generateToken } = require('../middleware/authJwt');
+const { generateToken, authenticateToken } = require('../middleware/authJwt');
 const bcrypt = require('bcrypt');
 
 const db = require('../models/db');
@@ -9,20 +9,24 @@ router.post('/', async (req, res) => {
     try {
         const email = req.body.email;
         const pass = req.body.password;
+
         console.log(email)
+
         const resp = await User.findOne({
             where: {
                 email: email,
             }
         })
         console.log(resp)
+
         if (!resp) {
             res.status(500).json({ err: true, msg: 'user does not exist.' });
             return;
         }
 
         var isValid = await bcrypt.compare(pass, resp.password);
-        if ( isValid) {
+
+        if (isValid) {
             res.json({ token: await generateToken(email), expiry: process.env.ACCESS_TOKEN_LIFE });
         } else { res.status(500).json({ err: true, msg: 'user does not exist' }); }
 
@@ -31,6 +35,10 @@ router.post('/', async (req, res) => {
         console.log(e);
         res.status(500).json({ err: true, msg: 'user does not exist' });
     }
+});
+
+router.get('/ping', authenticateToken, async (req, res) => {
+    res.send('poing');
 });
 
 module.exports = router;
