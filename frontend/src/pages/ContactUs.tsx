79a25eb48d3224098f92152a4459cd6c747e0668
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-04-06 02:26:18 +0530

    [WIP] Contact page

--- frontend/src/pages/ContactUs.tsx
+++ frontend/src/pages/ContactUs.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Contactus.css';
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField';
+import { Button, TextField, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme => ({
@@ -16,15 +15,45 @@ const useStyles = makeStyles((theme => ({
 
 function Contactus() {
     const classes = useStyles();
+    const [name, setName] = useState<String>('');
+    const [email, setEmail] = useState<String>('');
+    const [phone, setPhone] = useState<String>('');
+    const [msg, setMsg] = useState<String>('');
+
+    const submit = () => {
+
+    }
+
     return (
         <div className="Contactus">
 
-            <h1><b>Contact Us</b></h1>
-            <form className={classes.root} noValidate autoComplete="off">
+            {/* <h1><b>Contact Us</b></h1> */}
+            <Typography variant="h2">Contact Us</Typography>
+            <div className={classes.root} >
 
-                <TextField required id="standard-basic" label="Name" /><br></br><br></br>
-                <TextField required id="standard-basic" label="Email" /><br></br><br></br>
-                <TextField required id="standard-basic" label="Phone Number" /><br></br><br></br>
+                <TextField required
+                    id="standard-basic"
+                    label="Name"
+                    onChange={e => setName(e.target.value)}
+                />
+                <br></br><br></br>
+                <TextField
+                    required
+                    id="standard-basic"
+                    onChange={e => setEmail(e.target.value)}
+
+                    label="Email"
+                />
+                <br></br><br></br>
+                <TextField
+                    required
+                    id="standard-basic"
+                    label="Phone Number"
+                    onChange={e => setPhone(e.target.value)}
+                />
+                <br></br><br></br>
+
+
                 <TextField
                     id="outlined-multiline-static"
                     label="Your message"
@@ -32,10 +61,16 @@ function Contactus() {
                     required
                     rows={5}
                     variant="outlined"
+                    onChange={e => setMsg(e.target.value)}
+
                 />
-            </form>
+            </div>
             <br></br>
-            <Button variant="contained" color="primary">
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={e => submit()}
+            >
                 Submit
             </Button>
 
