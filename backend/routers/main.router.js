68a45a1d74c0a6434241445f71cd310415f28645
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-04 02:02:48 +0530

    feat: raw sql query

--- backend/routers/main.router.js
+++ backend/routers/main.router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const db = require('../models/db');
+const { QueryTypes } = require('sequelize');
+
 const { User, Gene, Interaction } = db;
 
 router.post('/drugSearch', async (req, res) => {
@@ -9,10 +11,10 @@ router.post('/drugSearch', async (req, res) => {
         console.log(search);
         let ans = [];
 
-        for(let i=0;i<search.length;i++){
+        for (let i = 0; i < search.length; i++) {
             let s = search[i];
-            let x = await Interaction.findAll({where: {gene: s}});
-            ans.push(...x); 
+            let x = await Interaction.findAll({ where: { gene: s } });
+            ans.push(...x);
         }
         res.json({ ans: ans });
     }
@@ -22,5 +24,21 @@ router.post('/drugSearch', async (req, res) => {
 
     }
 });
+
+router.post('/raw', async (req, res) => {
+    try {
+        const { query } = req.body;
+        console.log(`query: ${query}`);
+        if(query.length ==0){
+            return res.status(500).json({ err: true, msg: 'query too short' });
+
+        }
+        const result = await db.sequelize.query(query, { type: QueryTypes.SELECT });
+        res.json(result);
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ err: true, msg: 'invalid request' });
+    }
+})
 
 module.exports = router;
--- frontend/src/pages/RawQuery.tsx
+++ frontend/src/pages/RawQuery.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Swal from 'sweetalert2';
 import {
     Typography, TextField, Grid, Paper, Button,
     Accordion, AccordionSummary, TextareaAutosize,
@@ -7,7 +8,7 @@ import {
 
 import { makeStyles } from '@material-ui/core/styles';
 
-
+import axios from 'util/api';
 import {
     ExpandMore as ExpandMoreIcon,
     HelpOutline as HelpOutlineIcon
@@ -43,54 +44,79 @@ const useStyles = makeStyles((theme => ({
 const makeData = (a: String, b: String, c: String = '') => {
     return { column: a, datatype: b, description: c }
 }
+const targetComps = [
+    makeData('PID', 'STRING', 'Protein ID, Primary Key'),
+    makeData('accession', 'STRING', ''),
+    makeData('description', 'STRING', 'info about protein target'),
+    makeData('type', 'STRING', ''),
+    makeData('relationship', 'STRING', '')
+]
+
+const crossReferences = [
+    makeData('PID', 'STRING', ''),
+    makeData('xref_id', 'STRING'),
+    makeData('xref_name', 'STRING'),
+    makeData('xref_src', 'STRING')
+]
+
+const targetXref = [
+    makeData('PID', 'STRING'),
+    makeData('xrefid', 'STRING'),
+    makeData('name', 'STRING'),
+    makeData('src_db', 'STRING')
+]
+
+const targets = [
+    makeData('PID', 'STRING'),
+    makeData('organism', 'STRING'),
+    makeData('pref_name', 'STRING'),
+    makeData('score', 'STRING'),
+    makeData('target_type', 'STRING'),
+    makeData('tax_id', 'STRING'),
+    makeData('species_group_flag', 'STRING')
+]
+
+const synonyms = [
+    makeData('PID', 'STRING'),
+    makeData('name', 'STRING'),
+    makeData('type', 'STRING')
+]
+
+
+
 function RawQuery() {
+    const [query, setQuery] = React.useState<string>('');
+    const [ans, setAns] = React.useState<string>('');
     const classes = useStyles();
-    const targetComps = [
-        makeData('PID', 'STRING', 'Protein ID, Primary Key'),
-        makeData('accession', 'STRING', ''),
-        makeData('description', 'STRING', 'info about protein target'),
-        makeData('type', 'STRING', ''),
-        makeData('relationship', 'STRING', '')
-    ]
+    const submitQuery = async () => {
+        try {
+            const payload = {
+                query: query
+            }
 
-    const crossReferences = [
-        makeData('PID', 'STRING', ''),
-        makeData('xref_id', 'STRING'),
-        makeData('xref_name', 'STRING'),
-        makeData('xref_src', 'STRING')
-    ]
-
-    const targetXref = [
-        makeData('PID', 'STRING'),
-        makeData('xrefid', 'STRING'),
-        makeData('name', 'STRING'),
-        makeData('src_db', 'STRING')
-    ]
-
-    const targets = [
-        makeData('PID', 'STRING'),
-        makeData('organism', 'STRING'),
-        makeData('pref_name', 'STRING'),
-        makeData('score', 'STRING'),
-        makeData('target_type', 'STRING'),
-        makeData('tax_id', 'STRING'),
-        makeData('species_group_flag', 'STRING')
-    ]
-
-    const synonyms = [
-        makeData('PID', 'STRING'),
-        makeData('name', 'STRING'),
-        makeData('type', 'STRING')
-    ]
-
+            const result = await axios.post('/user/raw', payload);
+            console.log(result.status)
+            if (result.status == 200) {
+                console.log(result.data);
+                setAns(JSON.stringify(result.data));
+            }
+        }
+        catch (e) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: `${e}`,
+            });
+        }
+    }
     return (
         <div className={classes.root}>
             <Typography variant="h2">RAW SQL Query</Typography>
             <Tooltip placement="right" title="SQL Queries are READ only. Queries that contain MODIFY / UPDATE / DELETE / DROP / any query to modify the state of the table are not allowed.">
                 <ListItemIcon> <HelpOutlineIcon /></ListItemIcon>
             </Tooltip>
-            <Divider/>
-            <br/>
+            <Divider />
+            <br />
             <Grid container spacing={3}>
                 <Grid item xs={6}>
                     <Accordion TransitionProps={{ unmountOnExit: true }} >
@@ -172,19 +198,34 @@ function RawQuery() {
 
                     <TextareaAutosize
                         rowsMin={10}
+                        value={query}
+                        onChange={e => setQuery(e.target.value)}
                         className={classes.query}
                         aria-label="maximum height"
-                        placeholder="Maximum 4 rows"
-                        defaultValue='SELECT organism, type  FROM `TARGET_SYNONYMs` as syn  JOIN `TARGETs` as T WHERE T.PID="CHEMBL5064" AND syn.PID="CHEMBL5064" LIMIT 10;'
+                        placeholder='SELECT organism, type  FROM `TARGET_SYNONYMs` as syn  JOIN `TARGETs` as T WHERE T.PID="CHEMBL5064" AND syn.PID="CHEMBL5064" LIMIT 10;'
                     />
-                    <br/>
-                    <Button variant="contained" color="primary">
+                    <br />
+                    <Button variant="contained" color="primary"
+                        onClick={() => { submitQuery() }}
+                    >
                         Submit
                     </Button>
                 </Grid>
             </Grid>
 
-
+            <Typography variant="h4">
+                Results
+            </Typography>
+           
+                <TextareaAutosize
+                    rowsMin={10}
+                    value={ans}
+                    disabled={true}
+                    className={classes.query}
+                    aria-label="maximum height"
+                    placeholder="Maximum 4 rows"
+                />
+            
         </div>
 
         // </div>
