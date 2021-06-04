81e0dd7d82d5d5825558882899b4a790904cee0d
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-06-04 18:04:28 +0530

    [WIP] Gene submission

--- frontend/src/pages/SubmitProt.tsx
+++ frontend/src/pages/SubmitProt.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
     Typography, Grid, Paper,
-    TextareaAutosize
+    TextareaAutosize, TextField
 
 } from '@material-ui/core'
 import { setUserCurrentPage } from 'features/counter/counterSlice'
@@ -16,10 +16,27 @@ function BioCalc() {
             <Typography variant="h2">
                 Submit Gene Sequence
             </Typography>
+            <br /><br />
+            <Typography variant="h5">Submission Details</Typography>
+            <br /><br />
 
-            <Typography variant="subtitle1">
-                Work in progress
-            </Typography>
+            <TextField id="outlined-basic" label="Certification Number" variant="outlined" /><br /> <br />
+            <TextField id="outlined-basic" label="gene id" variant="outlined" /><br /> <br />
+            <TextField id="outlined-basic" label="gene name" variant="outlined" /><br /> <br />
+            <br /> 
+            <TextareaAutosize
+                rowsMin={10}
+
+                aria-label="maximum height"
+                placeholder="fasta sequence"
+            />
+            <br />
+            <Button 
+            color="primary"
+            variant="contained"
+            >
+                Submit
+            </Button>
         </div>
     )
 }
